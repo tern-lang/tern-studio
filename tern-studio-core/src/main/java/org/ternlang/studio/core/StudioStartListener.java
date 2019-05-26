f0223f666adf052feddb454b61acec087ab33946
Authrus <authrus.root@gmail.com>
2019-05-26 02:21:10 +0100

    Restructure packages

--- tern-studio-core/src/main/java/org/ternlang/studio/core/StudioStartListener.java
+++ tern-studio-core/src/main/java/org/ternlang/studio/core/StudioStartListener.java
@@ -5,7 +5,7 @@ import java.net.InetSocketAddress;
 import org.simpleframework.module.annotation.Component;
 import org.simpleframework.module.annotation.Value;
 import org.simpleframework.module.core.ComponentListener;
-import org.simpleframework.module.resource.Acceptor;
+import org.simpleframework.module.resource.server.Acceptor;
 import org.ternlang.studio.common.ProgressManager;
 
 import lombok.extern.slf4j.Slf4j;
