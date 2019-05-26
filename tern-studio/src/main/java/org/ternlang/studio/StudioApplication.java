bce9a37b3ccb615ff41252627437d483389e2abb
Authrus <authrus.root@gmail.com>
2019-05-26 02:10:42 +0100

    Update session cookie

--- tern-studio/src/main/java/org/ternlang/studio/StudioApplication.java
+++ tern-studio/src/main/java/org/ternlang/studio/StudioApplication.java
@@ -1,5 +1,7 @@
 package org.ternlang.studio;
 
+import static org.simpleframework.module.resource.SessionCookie.SESSION_ID;
+
 import org.simpleframework.module.ApplicationBuilder;
 import org.simpleframework.module.annotation.Import;
 import org.simpleframework.module.annotation.Module;
@@ -20,7 +22,7 @@ public class StudioApplication {
          .withModule(StudioApplication.class)
          .withArguments(list)
          .withName("Apache/2.2.14")
-         .withCookie("SESSID")
+         .withCookie(SESSION_ID)
          .withThreads(10)
          .start();
 
