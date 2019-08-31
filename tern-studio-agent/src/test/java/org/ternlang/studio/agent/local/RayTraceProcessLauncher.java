b0ff4e57b0d99bfd871881a33537ed636091cb22
Niall Gallagher <gallagher_niall@yahoo.com>
2019-08-31 17:08:36 +0100

    Add a time ticker test

--- tern-studio-agent/src/test/java/org/ternlang/studio/agent/local/RayTraceProcessLauncher.java
+++ tern-studio-agent/src/test/java/org/ternlang/studio/agent/local/RayTraceProcessLauncher.java
@@ -4,7 +4,7 @@ public class RayTraceProcessLauncher {
 
    public static void main(String[] list) throws Exception {
       LocalProcess.launch(
-              "--cp=../snap-studio/work/demo/misc/src",
+              "--cp=../tern-studio/work/demo/misc/src",
               "--s=ray_tracer_no_constraints.tern",
               "--v=true");
    }
