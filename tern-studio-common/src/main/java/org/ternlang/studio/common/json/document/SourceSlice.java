af18d734bd3798dfb9d413955f0f2944a9de5cc4
Niall Gallagher <gallagher_niall@yahoo.com>
2019-12-22 13:05:39 +0000

    Add hash code and equals to all slices

--- tern-studio-common/src/main/java/org/ternlang/studio/common/json/document/SourceSlice.java
+++ tern-studio-common/src/main/java/org/ternlang/studio/common/json/document/SourceSlice.java
@@ -5,11 +5,12 @@ public class SourceSlice implements CharSequence {
    private char[] source;
    private int off;
    private int length;
-   
+   private int hash;
+
    public SourceSlice() {
       super();
    }
-   
+
    public SourceSlice with(char[] source, int off, int length) {
       this.source = source;
       this.off = off;
@@ -24,36 +25,76 @@ public class SourceSlice implements CharSequence {
    public int offset() {
       return off;
    }
-   
+
    @Override
-   public int length() {      
+   public int length() {
       return length;
    }
-   
+
    @Override
-   public char charAt(int index) {      
+   public char charAt(int index) {
       return source[off + index];
    }
-   
+
    @Override
-   public CharSequence subSequence(int start, int end) {     
+   public CharSequence subSequence(int start, int end) {
       SourceSlice slice = new SourceSlice();
-            
-      if(end - start > 0) {      
+
+      if(end - start > 0) {
          return slice.with(source, off + start, end - start);
       }
       return slice;
    }
-   
+
+   @Override
+   public int hashCode() {
+      int local = hash;
+
+      if(local == 0 && length > 0) {
+         for(int i = 0; i < length; i++) {
+            local = 31 * local + source[off + i];
+         }
+         hash = local;
+      }
+      return local;
+   }
+
+   @Override
+   public boolean equals(Object other) {
+      if(other == null) {
+         return false;
+      }
+      if(other == this) {
+         return true;
+      }
+      if(other instanceof CharSequence) {
+         CharSequence text = (CharSequence) other;
+         int size = text.length();
+
+         if(size != length) {
+            return false;
+         }
+         for(int i = 0; i < size; i++) {
+            char next = text.charAt(i);
+
+            if(next != source[off + i]) {
+               return false;
+            }
+         }
+         return true;
+      }
+      return false;
+   }
+
    public boolean isEmpty() {
       return length <= 0;
    }
-   
+
    public void reset() {
-      off = length = 0;
+      off = length = hash = 0;
       source = null;
    }
-   
+
    @Override
    public String toString() {
       return new String(source, off, length);
