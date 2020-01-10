0fc734d2cedf90c4cab2d2110d4a2231152d438c
Niall Gallagher <gallagher_niall@yahoo.com>
2020-01-10 14:36:05 +0000

    Archive old work before delete

--- tern-studio-message/src/main/java/org/ternlang/studio/message/idl/codegen/build/EnumArrayGenerator.java
+++ tern-studio-message/src/main/java/org/ternlang/studio/message/idl/codegen/build/EnumArrayGenerator.java
@@ -1,5 +1,50 @@
 package org.ternlang.studio.message.idl.codegen.build;
 
-public class EnumArrayGenerator {
+import org.ternlang.studio.message.idl.Property;
+import org.ternlang.studio.message.idl.codegen.CodeAppender;
 
+public class EnumArrayGenerator implements PropertyGenerator {
+
+   @Override
+   public void generateField(CodeAppender builder, String owner, Property property) {
+      String name = property.getName();
+      String constraint = property.getConstraint();
+
+      builder.append("   private %sArrayCodec %s = new %sArrayCodec();\n", constraint, name, constraint);
+   }
+   
+   @Override
+   public void generateGetter(CodeAppender builder, String owner, Property property) {
+      String constraint = property.getConstraint();
+      String name = property.getName();
+      int length = property.getDimension();
+      int offset = property.getOffset();
+      
+      builder.append("   @Override\n");
+      builder.append("   public %sArrayBuilder %s() {\n", constraint, name);
+      builder.append("      %s.wrap(buffer, offset + %s, %s * Primitive.BYTE_SIZE);\n", name, offset, length);
+      builder.append("      return %s;\n", name);
+      builder.append("   }\n");
+   }
+
+   @Override
+   public void generateSetter(CodeAppender builder, String owner, Property property) {
+      // setter is a getter
+   }
+
+   @Override
+   public void generateGetterSignature(CodeAppender builder, String owner, Property property) {
+      String constraint = property.getConstraint();
+      String name = property.getName();
+      
+      builder.append("   %sArray %s();\n", constraint, name);
+   }
+
+   @Override
+   public void generateSetterSignature(CodeAppender builder, String owner, Property property) {
+      String constraint = property.getConstraint();
+      String name = property.getName();
+      
+      builder.append("   %sArrayBuilder %s();\n", constraint, name);
+   }
 }
--- tern-studio-message/src/main/java/org/ternlang/studio/message/idl/codegen/build/EnumGenerator.java
+++ tern-studio-message/src/main/java/org/ternlang/studio/message/idl/codegen/build/EnumGenerator.java
@@ -1,5 +1,52 @@
 package org.ternlang.studio.message.idl.codegen.build;
 
-public class EnumGenerator {
+import org.ternlang.studio.message.idl.Property;
+import org.ternlang.studio.message.idl.codegen.CodeAppender;
 
+public class EnumGenerator implements PropertyGenerator {
+
+   @Override
+   public void generateField(CodeAppender builder, String owner, Property property) {
+      // no field for primitive
+   }
+   
+   @Override
+   public void generateGetter(CodeAppender builder, String owner, Property property) {
+      String constraint = property.getConstraint();
+      String name = property.getName();
+      int offset = property.getOffset();
+      
+      builder.append("   @Override\n");
+      builder.append("   public %s %s() {\n", constraint, name);
+      builder.append("      return %s.resolve(buffer.getByte(offset + %s));\n", name, offset);
+      builder.append("   }\n");
+   }
+
+   @Override
+   public void generateSetter(CodeAppender builder, String owner, Property property) {
+      String constraint = property.getConstraint();
+      String name = property.getName();
+      int offset = property.getOffset();
+      
+      builder.append("   @Override\n");
+      builder.append("   public void %s(%s %s) {\n", name, constraint, name);
+      builder.append("      return buffer.setByte(offset + %s, %s.code);\n", offset, name);
+      builder.append("   }\n");
+   }
+
+   @Override
+   public void generateGetterSignature(CodeAppender builder, String owner, Property property) {
+      String constraint = property.getConstraint();
+      String name = property.getName();
+      
+      builder.append("   %s %s();\n", constraint, name);
+   }
+
+   @Override
+   public void generateSetterSignature(CodeAppender builder, String owner, Property property) {
+      String constraint = property.getConstraint();
+      String name = property.getName();
+      
+      builder.append("   void %s(%s %s);\n", name, constraint, name);
+   }
 }
